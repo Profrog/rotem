ecdbf072c016c19bdfa7904a578abcf0dd0593ac
profrog <ache159@naver.com>
2023-06-05 17:43:28 +0900

    Update 음셩변환.py

--- 음셩변환.py
+++ 음셩변환.py
@@ -11,15 +11,68 @@ mo_he_list = {'ㅏ' :['a',12623],'ㅐ' :['ae',12624],'ㅑ' :['ya',12625],'ㅒ' :
 
 
 
-txt1 = open('test.txt', 'w')
+list_len1 = {'a':['0', 0],'b':['0', 0],'c':['0', 0],'d':['0', 0],'e':['0', 0],'f':['0', 0],'g':['0', 0],'h':['0', 0],'i':['0', 0],'j':['0', 0],'k':['0', 0],'l':['0', 0],'m':['0', 0],'n':['0', 0],'o':['0', 0],'p':['0', 0],'q':['0', 0],'r':['0', 0],'s':['0', 0],
+'t':['0', 0],'u':['0', 0],'v':['0', 0],'w':['0', 0],'x':['0', 0],'y':['0', 0],'z':['0', 0]}
+
+list_len2 = {}
+list_len3 = {}
+
+
+for i in ja_he_list:
+ if(len(ja_he_list[i][0]) == 3):
+  list_len3[ja_he_list[i][0]] = [i,ja_he_list[i][1]]
+  
+ elif(len(ja_he_list[i][0]) == 2):
+  list_len2[ja_he_list[i][0]] = [i,ja_he_list[i][1]]
+
+ else:
+  list_len1[ja_he_list[i][0]] = [i,ja_he_list[i][1]]
+  
+for i in mo_he_list:
+ if(len(mo_he_list[i][0]) == 3):
+  list_len3[mo_he_list[i][0]] = [i,mo_he_list[i][1]]
+  
+ elif(len(mo_he_list[i][0]) == 2):
+  list_len2[mo_he_list[i][0]] = [i,mo_he_list[i][1]]
+
+ else:
+  list_len1[mo_he_list[i][0]] = [i,mo_he_list[i][1]]
+  
+  
+print(list_len3)
+print(list_len2)
+print(list_len1)
+
+txt1 = open('test1.txt', 'w')
+txt1.write('{')
+
+txt2 = open('text2.txt', 'w')
+txt2.write('{')
+
+txt3 = open('text3.txt', 'w')
+txt3.write('{')
+
+for i in list_len3:
+ txt3.write('\'' + i +  '\' :[\'' + list_len3[i][0] + '\',' + str(list_len3[i][1]) + '],')
+
+for i in list_len2:
+ txt2.write('\'' + i +  '\' :[\'' + list_len2[i][0] + '\',' + str(list_len2[i][1]) + '],')
+
+for i in list_len1:
+ txt1.write('\'' + i +  '\' :[\'' + list_len1[i][0] + '\',' + str(list_len1[i][1]) + '],')
+
+
+txt1.write('}')
+txt2.write('}')
+txt3.write('}')
 
 
 #for i in range(12593, 12594):
  #print(chr(i), end = ' ')
  #txt1.write('\'' +  chr(i) +  '\' :\'\',')
 
-for i in mo_he_list:
- print(i + " " + chr(mo_he_list[i][1]))
+#for i in mo_he_list:
+ #print(i + " " + chr(mo_he_list[i][1]))
  
 
 #close(txt1) 
