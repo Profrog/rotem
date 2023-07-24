1453e9d7d92217d0ce1ddb510326f631b345c8b6
profrog <ache159@naver.com>
2023-07-24 14:31:58 +0900

    Update 광물캐기.c

--- 광물캐기.c
+++ 광물캐기.c
@@ -1,45 +1,37 @@
-/**
-    CopyRight ⓒ 2020. Hyundai Rotem Co., Ltd All Rights Reserved  
-*/
-/**
-    @file       ATOTSComm.h
-    @brief      file description
-    @remark     Project - ETCS_ATO_CPU_APP \n
-                Development Environment - PC \n
-                Operating System - VxWorks 7 Cert \n
-                Target(CPU) - I.MX6 Solo \n
-                Language  - C \n
-    @note
-        2020.04.10(1)   ksw     First Issue \n
-*/
-
 /* includes */
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
 /* define*/
-#define m_n 3 /*광물의 종류 0 = diamond, 1 = iron, 2 = stone*/
-const char* mine_list[m_n] = {"diamond", "iron", "stone"};
+#define MINE_KIND 3 /*광물의 종류 0 = diamond, 1 = iron, 2 = stone*/
+const char* mine_list[MINE_KIND] = {"diamond", "iron", "stone"};
 
 /* typedef */
 typedef struct value0
 {
-  int mh_score[m_n];
-  int mine_num;
+  int mh_score[MINE_KIND];
+  int iMineNum;
 }VALUE0;
 
 /* global variables */
-int mine_map[m_n][m_n] = {{1,1,1}, {5,1,1}, {25,5,1}};
+int mine_map[MINE_KIND][MINE_KIND] = {{1,1,1}, {5,1,1}, {25,5,1}};
 int* pick_list = {0};
 
 
-/* 채취 광물 별 index 부여 */
-int check_mine(char* name)
+/* 
+
+@fn     int CheckMine(char* mineName)
+@brief  광물이름에 해당하는 인덱스값을 리턴한다(diamond = 0 , iron = 1 , stone = 2 )
+@param  mineName : 광물명
+@return i :  광물 인덱스 값, -1 : 에러코드
+
+*/
+int CheckMine(char* mineName)
 {
-  for(int i = 0; i < m_n ; ++i) /* diamond = 0 , iron = 1 , stone = 2 */
+  for(int i = 0; i < MINE_KIND ; ++i) 
   {
-    if(*name == *mine_list[i])
+    if(*mineName == *mine_list[i])
     {
         return i;
     }
@@ -48,11 +40,20 @@ int check_mine(char* name)
   return -1;
 }
 
-int* check_picks(int* picks,int picks_size) /* diamond > iron > stone 의 우선순위를 가지는 곡괭이 리스트 만들기 */
-{
-  int* pick_list0 = calloc(picks_size, sizeof(int));
 
-  for(int i = 0, k = 0; i < m_n; ++i) /* 각 곡괭이의 개수만큼의 길이를 가짐 */
+/*
+
+@fn int* CheckPicks(int* picks,int iPicksSize)
+@brief  diamond > iron > stone 의 우선순위를 가지는 인덱스 값을 곡괭이 개수 만큼 가지는 array를 만든다.
+@param  picks : 곡괭이_개수 배열, iPicksSize : 모든 종류의 곡괭이 개수의 합
+@return pick_list0 : 곡괭이 인덱스_곡괭이 인덱스 개수 배열
+
+*/
+int* CheckPicks(int* picks,int iPicksSize)
+{
+  int* pick_list0 = calloc(iPicksSize, sizeof(int));
+
+  for(int i = 0, k = 0; i < MINE_KIND; ++i) /* 각 곡괭이의 개수만큼의 길이를 가짐 */
   {
     for(int j = 0 ; j < picks[i]; ++j, ++k)
     {
@@ -64,40 +65,52 @@ int* check_picks(int* picks,int picks_size) /* diamond > iron > stone 의 우선
 }
 
 
+
+/* 
+
+@fn int solution(int picks[], size_t picks_len, const char* minerals[], size_t minerals_len)
+@brief  phase 1 : 곡괭이 3종류에 대하여 , 5개들이 묶음을 해당 곡괭이로 캤을때 비용계산
+        phase 2 : 지정된 5개 들이 묶음을 최적의 곡괭이에 배치
+        phase 3 : 배치된 비용들의 합을 계산
+@param  곡괭이 종류 및 개수, 광물 종류 및 개수
+@return iAnswer : 최적 비용계산
+*/
 int solution(int picks[], size_t picks_len, const char* minerals[], size_t minerals_len)
 {
-   int answer = 0; 
-   int dummy_size = (minerals_len/5) + (minerals_len%5 > 0?1:0);
-   int all_picks = picks[0] + picks[1] + picks[2];
-   int runout_t = (dummy_size < all_picks)? dummy_size : all_picks;
+   int iAnswer = 0; 
+   int iDummySize = (minerals_len/5) + (minerals_len%5 > 0?1:0);
+   int iAllPicks = picks[0] + picks[1] + picks[2];
+   int iRunoutT = (iDummySize < iAllPicks)? iDummySize : iAllPicks;
 
-   VALUE0* value0 = calloc(dummy_size, sizeof(VALUE0));
-   int* pick_list = check_picks(picks, all_picks);
+   VALUE0* value0 = calloc(iDummySize, sizeof(VALUE0));
+   int* pick_list = CheckPicks(picks, iAllPicks);
 
-   for(int i = 0, k = 0; (i < dummy_size) && (k < minerals_len); ++i) /* 각각의 광물에 대해 곡괭이 별로 얼마만큼의 비용이 필요한지 계산 */
+  /*phase 1*/
+   for(int i = 0, k = 0; (i < iDummySize) && (k < minerals_len); ++i) /* i : 5개들이 광물 컨트롤, k : 전체 광물 컨트롤 */
   {
    {
-    for(int j = 0; (j < 5) && (k < minerals_len) ; ++j, ++k)
+    for(int j = 0; (j < 5) && (k < minerals_len) ; ++j, ++k) /*j : 5개 들이 내부의 광물 컨트롤 */
     {
-       int index2 =  check_mine(minerals[k]);
+       int index2 =  CheckMine(minerals[k]);
        
-       for(int k = 0; k < m_n; ++k) /*곡괭이 종류 별로 캤을 때 비용을 저장*/
+       for(int k = 0; k < MINE_KIND; ++k) /*곡괭이 별로 캤을 때 광물 비용을 저장*/
         {
           value0[i].mh_score[k] += mine_map[k][index2];
         }
 
-        ++value0[i].mine_num;
+        ++value0[i].iMineNum;
     }
-
    }
+   /*phase 1*/
 
-   for(int i = 0; i < runout_t; ++i) /* 비용이 클 수록 비싼 곡괭이를 쓴다*/
+  /*phase 2*/
+   for(int i = 0; i < iRunoutT; ++i) /* i : 광물 5개 들이 묶음이나 곡괭이 개수 중 더 작은 쪽*/
    {
-    for(int j = i+1; j < runout_t; ++j)
+    for(int j = i+1; j < iRunoutT; ++j) /* j : 광물 5개 들이 묶음이나 곡괭이 개수 중 더 작은 쪽*/
     { 
-         for(int k = m_n-1; k >= 0; --k) /* 다만 비용이 같을 때는 개수가 적은 곡괭이가 우선*/
+         for(int k = MINE_KIND-1; k >= 0; --k) /*k : 다이아몬드 -> 철 -> 돌 */
          {
-          if(value0[i].mh_score[k] - value0[i].mine_num < value0[j].mh_score[k] - value0[j].mine_num)
+          if(value0[i].mh_score[k] - value0[i].iMineNum < value0[j].mh_score[k] - value0[j].iMineNum)
           {
             VALUE0 change = value0[i];
             value0[i] = value0[j];
@@ -107,11 +120,15 @@ int solution(int picks[], size_t picks_len, const char* minerals[], size_t miner
          }
     }
    }
-   
-   for(int i = 0; i < runout_t; ++i) /* 선정된 곡괭이의 광물에 대한 비용 합계 계산*/
+  }
+  /*phase 2*/
+
+   /*phase 3*/
+   for(int i = 0; i < iRunoutT; ++i) /* i : 광물 5개 들이 묶음이나 곡괭이 개수 중 더 작은 쪽*/
    {
-    answer += value0[i].mh_score[pick_list[i]];
+    iAnswer += value0[i].mh_score[pick_list[i]];
    }
+   /*phase 3*/
   
-    return answer;
+    return iAnswer;
 }
